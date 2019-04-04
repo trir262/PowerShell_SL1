736cf8f8b7d9b4ef5bf912e453c47109fb9d6675
Tom Robijns <Tom.Robijns@realdolmen.com>
2019-04-04 13:21:16 +0200

    Improve Connect-SL1 Tests

--- ScienceLogic_SL1/tests/functions/Connect-SL1.Tests.ps1
+++ ScienceLogic_SL1/tests/functions/Connect-SL1.Tests.ps1
@@ -11,22 +11,21 @@ InModuleScope 'ScienceLogic_SL1' {
         }
 
         Context 'Input Validation' {
-            $MandatoryParams = @('Uri', 'Credential')
-            $NonMandatoryParams = @('Passthru')
-
-            foreach ($MandatoryParam in $MandatoryParams) {
-                It "$($MandatoryParam) is mandatory" {
-                    (Get-Command Connect-SL1).Parameters[$MandatoryParam].Attributes.mandatory | Should -BeTrue
-                }
+            $MandatoryParams = @{param='Uri'},@{param='Credential'}
+            $NonMandatoryParams = @{param='Passthru'}
+            $MandatoryParams | Out-Null
+            $NonMandatoryParams | Out-Null
+            It "Parameter <param> is mandatory" -TestCases $MandatoryParams {
+                param($Param)
+                (Get-Command Connect-SL1).Parameters[$Param].Attributes.mandatory | Should -Be $True
             }
 
-            foreach ($NonMandatoryParam in $NonMandatoryParams) {
-                It "$($NonMandatoryParam) parameter is mandatory" {
-                    (get-command Connect-SL1).Parameters[$NonMandatoryParam].Attributes.mandatory | Should -BeFalse
-                }
+            It "Parameter <param> parameter is not mandatory" -TestCases $NonMandatoryParams {
+                param($Param)
+                (get-command Connect-SL1).Parameters[$Param].Attributes.mandatory | Should -BeFalse
             }
         }
-        Context 'Function Execution' {
+        Context 'Function Execution with Mandatory parameters' {
             BeforeAll {
                 Mock Invoke-SL1Request {
                     return New-Object -TypeName psobject -Property @{StatusCode = 200}
@@ -39,12 +38,18 @@ InModuleScope 'ScienceLogic_SL1' {
             It "Calls Invoke-SL1Request 1 time" {
                 Assert-MockCalled -CommandName Invoke-SL1Request -Exactly 1
             }
-
+        }
+        Context 'Function Execution With Passthru parameter' {
+            BeforeAll {
+                Mock Invoke-SL1Request {
+                    return New-Object -TypeName psobject -Property @{StatusCode = 200}
+                }
+            }
             It 'Outputs the connection information with passthru' {
                 Connect-SL1 -Uri $uri -Credential $Cred -Passthru | Should -Not -Be $Null
             }
-            It "Calls Invoke-SL1Request 2 times" {
-                Assert-MockCalled -CommandName Invoke-SL1Request -Exactly 2
+            It "Calls Invoke-SL1Request 1 time" {
+                Assert-MockCalled -CommandName Invoke-SL1Request -Exactly 1
             }
 
     }
