5c41154fea6f205feb464fd5b7aa3a3360befc42
Tom Robijns <Tom.Robijns@realdolmen.com>
2019-04-04 09:39:42 +0200

    First TDD Connect-SL1

--- ScienceLogic_SL1/tests/functions/Connect-SL1.Tests.ps1
+++ ScienceLogic_SL1/tests/functions/Connect-SL1.Tests.ps1
@@ -1,9 +1,45 @@
+InModuleScope 'ScienceLogic_SL1' {
+    Describe 'Connect-SL1' {
+        BeforeAll {
+            $Uri = 'https://www.google.be'
+            $Cred = [pscredential]::new('user', (ConvertTo-SecureString -AsPlainText -Force -String 'pwd') )
+            mock Invoke-SL1Request {
+                return New-Object -TypeName psobject -Property @{StatusCode = 200}
+            }
+        }
+
+        Context 'Input' {
+            $MandatoryParams = @('Uri', 'Credential')
+            $NonMandatoryParams = @('Passthru')
+
+            foreach ($MandatoryParam in $MandatoryParams) {
+                It "$($MandatoryParam) is mandatory" {
+                    (Get-Command Connect-SL1).Parameters[$MandatoryParam].Attributes.mandatory | Should -BeTrue
+                }
+            }
+
+            foreach ($NonMandatoryParam in $NonMandatoryParams) {
+                It "$($NonMandatoryParam) parameter is mandatory" {
+                    (get-command Connect-SL1).Parameters[$NonMandatoryParam].Attributes.mandatory | Should -BeFalse
+                }
+            }
+        }
+        Context 'Execution' {
+            It 'does not output anything' {
+                Connect-SL1 -Uri $uri -Credential $Cred | should be $null
+            }
+            It 'outputs the connection information with passthru' {
+                Connect-SL1 -Uri $uri -Credential $Cred -Passthru | should not be $null
+            }
+        }
+    }
+}
+<#
+$moduleRoot = (Resolve-Path "$PSScriptRoot\..\..").Path
+$manifest = ((Get-Content "$moduleRoot\ScienceLogic_SL1.psd1") -join "`n") | Invoke-Expression
 Describe "Testing Connect-SL1" {
-    $moduleRoot = (Resolve-Path "$PSScriptRoot\..\..").Path
-    $manifest = ((Get-Content "$moduleRoot\ScienceLogic_SL1.psd1") -join "`n") | Invoke-Expression
-        
     InModuleScope ScienceLogic_SL1 {
-        function Invoke-SL1Request {
+        mock function Invoke-SL1Request {
             return New-Object -TypeName psobject -Property @{StatusCode = 200}
         }
     }
@@ -12,10 +48,9 @@ Describe "Testing Connect-SL1" {
             $Uri = 'https://www.google.be'
             $Cred = [pscredential]::new('user', (ConvertTo-SecureString -AsPlainText -Force -String 'pwd') )
         }
-
-        it 'should return $null' {
-            Connect-SL1 -Uri $Uri -Credential $Cred -Passthru | should be Empty
+            it 'should return $null' {
+                Connect-SL1 -Uri $Uri -Credential $Cred -Passthru | should be Empty
         }
-
     }
 }
+#>
