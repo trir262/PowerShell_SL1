041fa431c67b665e3cb40588d482f4f3d5de2340
Tom Robijns <Tom.Robijns@realdolmen.com>
2019-04-13 22:53:23 +0200

    Tests for DeviceGroup and Discovery Session

---
+++ ScienceLogic_SL1/tests/functions/Get-SL1Discovery.Tests.ps1
@@ -0,0 +1,76 @@
+Write-PSFMessage -Level Critical -Message "Reimporting ScienceLogic_SL1 module"
+Remove-Module ScienceLogic_SL1 -ErrorAction Ignore
+Import-Module "$PSScriptRoot\..\..\ScienceLogic_SL1.psd1"
+$global:JSONs = . "$PSScriptRoot\JsonTemplates.ps1"
+InModuleScope 'ScienceLogic_SL1' {
+    Describe 'Get-SL1Discovery' {
+        BeforeAll {
+            $Url = 'https://www.someSL1url.com'
+            $BadCred = [pscredential]::new('Badcred', (ConvertTo-SecureString -AsPlainText -Force -String 'badpwd'))
+            $GoodCred = [pscredential]::new('goodcred', (ConvertTo-SecureString -AsPlainText -Force -String 'goodpwd') )
+            $Filter = 'filter.0._id.eq=492'
+            $Limit=100
+            # Mock Connect-SL1
+            Mock Invoke-SL1Request { 
+                return New-Object -TypeName psobject -Property @{ 
+                    StatusCode = [System.Net.HttpStatusCode]::OK 
+                } 
+            } -ParameterFilter { $Uri -match '/api/account/_self'}
+
+            #Mock for /api/appliance/<id>
+            Mock Invoke-SL1Request { 
+                return New-Object -TypeName psobject -Property @{ 
+                    StatusCode = [System.Net.HttpStatusCode]::OK
+                    Content = $global:JSONs['discovery_sessionID']
+                } 
+            } -ParameterFilter { $Uri -and $Uri -match '/api/discovery_session/\d+$' }
+            
+            #Mock for /api/appliance?limit
+            Mock Invoke-SL1Request { 
+                return New-Object -TypeName psobject -Property @{ 
+                    StatusCode = [System.Net.HttpStatusCode]::OK
+                    Content = ($global:JSONs['discovery_sessionquery'])
+                }
+            } -ParameterFilter { $Uri -and $Uri -eq "$($url)/api/discovery_session?$($Filter)&limit=$($Limit)"}
+
+            #Mock for /api/organization? contains=customerX&extended_fetch=1
+            Mock Invoke-SL1Request { 
+                return New-Object -TypeName psobject -Property @{ 
+                    StatusCode = [System.Net.HttpStatusCode]::OK
+                    Content = $global:JSONs['discovery_sessionxtend']
+                } # /api/organization?$($Filter)&limit=$($Limit)&hide_filterinfo=1&extended_fetch=1
+            } -ParameterFilter { $Uri -and $Uri -eq "$($Url)/api/discovery_session?$($Filter)&limit=$($limit)&hide_filterinfo=1&extended_fetch=1"}
+
+            Connect-SL1 -Uri $Url -Credential $GoodCred
+            $Cmd = 'Get-SL1Discovery'
+        }
+
+        Context 'Input Validation' {
+            $NonMandatoryParams = @{param='Id'},@{param='Limit'},@{param='Filter'}
+
+            It "Parameter <param> parameter is not mandatory" -TestCases $NonMandatoryParams {
+                param($Param)
+                (get-command $Cmd).Parameters[$Param].Attributes.mandatory | Should -Be $False
+            }
+        }
+
+        Context 'Testing Id Parameter' {
+            It 'Parameter Id is higher than 0 returns Discovery Session' {
+                Get-SL1Discovery -Id 1 | should -not -be $null
+                Assert-MockCalled -CommandName Invoke-SL1Request -Times 1 -Scope It
+            }
+
+            It 'Parameter Id is below 0 throws error' {
+                { Get-SL1Discovery -Id -5} | Should -Throw
+                Assert-MockCalled -CommandName Invoke-SL1Request -Times 0 -Scope It
+            }
+        }
+
+        Context 'Testing Filter Parameter' {
+            It 'Parameter Filter returns 1 Discovery Session' {
+                (Get-SL1Discovery -Filter $Filter).name | Should -be 'discovery'
+                Assert-MockCalled -CommandName Invoke-SL1Request -Times 2 -Scope It
+            }
+        }
+    }
+}
