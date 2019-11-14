c89e6bc1f948ec31574b22a2f181320c4ebea470
Tom Robijns <Tom.Robijns@realdolmen.com>
2019-11-15 00:18:28 +0100

    hoppa

--- ScienceLogic_SL1/Deploy.PSDeploy.ps1
+++ ScienceLogic_SL1/Deploy.PSDeploy.ps1
@@ -1,6 +1,6 @@
 ﻿Deploy Module {
     By PSGalleryModule {
-        FromSource "C:\projects\powershell-sl1\ScienceLogic_SL1\Artifacts"
+        FromSource "C:\projects\powershell-sl1\ScienceLogic_SL1\Artifacts\ScienceLogic_SL1"
         To PSGallery
         WithOptions @{
             ApiKey = $Env:NugetApiKey
